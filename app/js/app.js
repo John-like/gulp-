bfc9d4c7a900239154c5f3453c4c8c967f539fa0
john <136798718@qq.com>
2017-02-20 10:49:52 +0800

    Create app.js

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+var human = {
+    breathe() {
+        console.log('breathing...');
+    }
+};
+var worker = {
+    __proto__: human, //设置此对象的原型为human,相当于继承human
+    company: 'freelancer',
+    work() {
+        console.log('working...');
+    }
+};
+human.breathe();//输出 ‘breathing...’
+//调用继承来的breathe方法
+worker.breathe();//输出 ‘breathing...’
+
+ let a=1;
+ 
+var num=Math.random();
+//将这个数字输出到console
+console.log(`your num is ${num}`);
